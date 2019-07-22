e1177caf4378cab07e13624bb400ff7a6bc40d5e
OGU4 <53147458+OGU4@users.noreply.github.com>
2019-07-22 16:05:34 +0900

    Update portal-candidate-viewer.user.js

--- portal-candidate-viewer.user.js
+++ portal-candidate-viewer.user.js
@@ -3,7 +3,7 @@
 // @name           IITC-ja plugin: Portal Candidate Viewer
 // @author         nmmr
 // @category       Layer
-// @version        0.0.11
+// @version        0.0.12
 // @namespace      https://github.com/OGU4/1test
 // @updateURL      https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
 // @downloadURL    https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
@@ -19,7 +19,3 @@
 // @require        
 // @grant          GM_xmlhttpRequest
 // ==/UserScript==
-
-function wrapper(plugin_info) {
-// ensure plugin framework is there, even if iitc is not yet loaded
-if(typeof window.plugin !== 'function') window.plugin = function() {};
