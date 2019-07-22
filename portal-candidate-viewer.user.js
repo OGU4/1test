e4a3a61a35576a4d9c8703cfe3d71ad20d7ae0bc
OGU4 <53147458+OGU4@users.noreply.github.com>
2019-07-22 14:14:29 +0900

    Update portal-candidate-viewer.user.js

--- portal-candidate-viewer.user.js
+++ portal-candidate-viewer.user.js
@@ -1,10 +1,12 @@
 // ==UserScript==
 // @id             iitc-plugin-portal-candidate-viewer
 // @name           IITC-ja plugin: Portal Candidate Viewer
+// @author         nmmr
 // @category       Layer
 // @version        0.0.4
 // @namespace      https://sites.google.com/site/stocksite123456/
-// @downloadURL
+// @updateURL      https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
+// @downloadURL    https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
 // @description    Show portal candidate on the map.
 // @include        https://intel.ingress.com/intel*
 // @match          https://intel.ingress.com/intel*
