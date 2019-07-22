ebb73322d2c5241d7ff59fa1881fa193c3f03840
OGU4 <53147458+OGU4@users.noreply.github.com>
2019-07-22 15:55:27 +0900

    Update portal-candidate-viewer.user.js

--- portal-candidate-viewer.user.js
+++ portal-candidate-viewer.user.js
@@ -3,8 +3,8 @@
 // @name           IITC-ja plugin: Portal Candidate Viewer
 // @author         nmmr
 // @category       Layer
-// @version        0.0.
-// @namespace      https://sites.google.com/site/stocksite123456/
+// @version        0.0.10
+// @namespace      https://github.com/OGU4/1test
 // @updateURL      https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
 // @downloadURL    https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
 // @description    Show portal candidate on the map.
@@ -19,3 +19,14 @@
 // @require        
 // @grant          GM_xmlhttpRequest
 // ==/UserScript==
+
+function wrapper(plugin_info) {
+// ensure plugin framework is there, even if iitc is not yet loaded
+if(typeof window.plugin !== 'function') window.plugin = function() {};
+
+//PLUGIN AUTHORS: writing a plugin outside of the IITC build environment? if so, delete these lines!!
+//(leaving them in place might break the 'About IITC' page or break update checks)
+plugin_info.buildName = 'portal-candidate-viewer';
+plugin_info.dateTimeVersion = '20190722';
+plugin_info.pluginId = 'portal-candidate-viewer';
+//END PLUGIN AUTHORS NOTE
