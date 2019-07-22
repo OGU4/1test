e3e9183e04386159107ec689bcaec0ad8d079462
OGU4 <53147458+OGU4@users.noreply.github.com>
2019-07-22 15:22:48 +0900

    Update portal-candidate-viewer.user.js

--- portal-candidate-viewer.user.js
+++ portal-candidate-viewer.user.js
@@ -9,9 +9,13 @@
 // @downloadURL    https://raw.githubusercontent.com/OGU4/1test/master/portal-candidate-viewer.user.js
 // @description    Show portal candidate on the map.
 // @include        https://intel.ingress.com/intel*
+// @include        http://intel.ingress.com/intel*
 // @match          https://intel.ingress.com/intel*
+// @match          http://intel.ingress.com/intel*
 // @include        https://intel.ingress.com/mission/*
+// @include        http://intel.ingress.com/mission/*
 // @match          https://intel.ingress.com/mission/*
+// @match          http://intel.ingress.com/mission/*
 // @require        
 // @grant          GM_xmlhttpRequest
 // ==/UserScript==
